02391fd83fb5a25a476774233a3d22bc9fe9a7b1
Edian Comachio <comachio.edian@gmail.com>
2025-02-02 18:13:35 -0300

    general improvements

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { motion } from "framer-motion"
 
 import { default as RadioPlayerComponent } from "./RadioPlayer"
 import 'react-h5-audio-player/lib/styles.css';
+import logo from '/logo.png';
+import { GithubIcon } from "lucide-react";
 
 export default function App() {
   return (
-    <main className="flex w-full min-h-screen mt-32 flex-col items-center justify-center p-24 relative overflow-hidden">
+    <main className="flex w-full min-h-screen mt-16 sm:mt-12 flex-col items-center p-4 sm:p-24 relative overflow-hidden">
       <motion.div
         className="absolute inset-0 -z-10"
         initial={{ backgroundPosition: "0% 0%" }}
@@ -17,14 +19,16 @@ export default function App() {
           backgroundSize: "60px 60px",
         }}
       />
-      <motion.h1
-        className="text-4xl font-bold mb-8"
-        initial={{ opacity: 0, y: -20 }}
+      <motion.div
+        className="flex flex-col items-center justify-center w-full px-4 sm:px-0"
+        initial={{ opacity: 0, y: -30 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5 }}
       >
-        Gaucha Radio com Delay
-      </motion.h1>
+        <img src={logo} alt="Rádio Gaúcha" className="w-3/4 sm:w-1/2 mb-2" />
+        <h1 className="text-2xl sm:text-3xl font-bold mb-2">Radio com Delay</h1>
+
+      </motion.div>
       <RadioPlayerComponent />
       {/* <RadioPlayer /> */}
       {/* <ReactPlayer url='https://liverdgaupoa.rbsdirect.com.br/primary/gaucha_rbs.sdp/playlist.m3u8' /> */}
@@ -36,51 +40,51 @@ export default function App() {
       /> */}
       {/* explanation div */}
       <motion.div
-        className="w-full max-w-xl p-8 mt-16 bg-white rounded-lg shadow-lg space-y-6"
+        className="w-full max-w-xl p-4 sm:p-8 mt-8 sm:mt-16 bg-white rounded-lg shadow-lg space-y-4 sm:space-y-6"
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5 }}
       >
-        <section className="space-y-3">
-          <p className="text-gray-700">
+        <section className="space-y-2 sm:space-y-3">
+          <p className="text-gray-700 text-sm sm:text-base">
             Sincronizar jogos de futebol com a transmissão da TV e ouvir a narração ao vivo
           </p>
         </section>
 
-        <section className="space-y-3">
-          <h2 className="text-2xl font-bold text-slate-600">Como usar</h2>
+        <section className="space-y-2 sm:space-y-3">
+          <h2 className="text-xl sm:text-2xl font-bold text-slate-600">Como usar</h2>
           <div className="text-gray-700 space-y-2">
             <p className="flex gap-2">
-              <span className="flex items-center justify-center w-6 h-6 bg-slate-100 rounded-full text-slate-600 font-semibold">1</span>
+              <span className="flex flex-shrink-0 items-center justify-center w-6 h-6 bg-slate-100 rounded-full text-slate-600 font-semibold">1</span>
               Clique no botão de pause para pausar a transmissão.
             </p>
             <p className="flex gap-2">
-              <span className="flex items-center justify-center w-6 h-6 bg-slate-100 rounded-full text-slate-600 font-semibold">2</span>
+              <span className="flex flex-shrink-0 items-center justify-center w-6 h-6 bg-slate-100 rounded-full text-slate-600 font-semibold">2</span>
               Ajuste o tempo de delay para o tempo em que o jogo começa.
             </p>
             <p className="flex gap-2">
-              <span className="flex items-center justify-center w-6 h-6 bg-slate-100 rounded-full text-slate-600 font-semibold">3</span>
+              <span className="flex flex-shrink-0 items-center justify-center w-6 h-6 bg-slate-100 rounded-full text-slate-600 font-semibold">3</span>
               Clique no botão de play para continuar a transmissão.
             </p>
           </div>
         </section>
 
-        <section className="space-y-3">
-          <h2 className="text-2xl font-bold text-slate-600">Dicas</h2>
+        <section className="space-y-2 sm:space-y-3">
+          <h2 className="text-xl sm:text-2xl font-bold text-slate-600">Dicas</h2>
           <div className="text-gray-700 space-y-2">
             <p className="flex gap-2">
               <span className="flex flex-shrink-0 items-center justify-center w-6 h-6 bg-slate-100 rounded-full text-slate-600 font-semibold">1</span>
               Identifique um momento marcante do jogo para pausar a transmissão. Por exemplo, um lateral, recuo para o goleiro, etc.
             </p>
             <p className="flex gap-2">
-              <span className="flex items-center justify-center w-6 h-6 bg-slate-100 rounded-full text-slate-600 font-semibold">2</span>
+              <span className="flex flex-shrink-0 items-center justify-center w-6 h-6 bg-slate-100 rounded-full text-slate-600 font-semibold">2</span>
               Espere o mesmo momento ocorrer na TV e clique no botão de play.
             </p>
           </div>
         </section>
 
-        <section className="space-y-3">
-          <h2 className="text-2xl font-bold text-slate-600">Como funciona</h2>
+        <section className="space-y-2 sm:space-y-3">
+          <h2 className="text-xl sm:text-2xl font-bold text-slate-600">Como funciona</h2>
           <p className="text-gray-700 leading-relaxed">
             Ao clicar no botão de pause, o player irá pausar a transmissão por um tempo determinado pelo usuário.
             Ao clicar no botão de play, o player irá continuar a transmissão a partir do ponto em que foi pausada.
@@ -88,6 +92,12 @@ export default function App() {
         </section>
       </motion.div>
 
+      <div className="flex gap-2 mt-4">
+        <a href="https://github.com/ecomachio/delayedradioapp" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2">
+          <GithubIcon className="w-6 h-6 text-slate-600" />
+          Github
+        </a>
+      </div>
     </main>
   )
 }
--- src/RadioPlayer.tsx
+++ src/RadioPlayer.tsx
@@ -1,6 +1,9 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import { motion, AnimatePresence } from "framer-motion"
 import React, { useEffect, useRef, useState } from 'react';
 import Hls from 'hls.js';
+import { Button } from "./components/ui/button";
+import { FastForward, Pause, Play, Rewind } from "lucide-react";
 
 function RadioPlayer() {
   const audioRef = useRef<HTMLAudioElement | null>(null);
@@ -9,7 +12,7 @@ function RadioPlayer() {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const [_, setElapsedPauseTime] = useState<any>(0);
   const [pauseInterval, setPauseInterval] = useState<number | null>(null);
-
+  const [delay, setDelay] = useState<number>(0);
 
   useEffect(() => {
     const audio = audioRef.current;
@@ -22,7 +25,7 @@ function RadioPlayer() {
       // In a dynamic production environment, you might compute or fetch this URL.
       hls.loadSource('https://liverdgaupoa.rbsdirect.com.br/primary/gaucha_rbs.sdp/playlist.m3u8');
       hls.attachMedia(audio);
-      
+
       // Cleanup on component unmount
       return () => {
         hls.destroy();
@@ -100,13 +103,117 @@ function RadioPlayer() {
     };
   }, [pauseTimeout]);
 
+  const handleAudioChange = () => {
+    const delay = Math.round(audioRef.current!.currentTime! - audioRef.current!.duration!) + 5;
+    if (delay > 0) {
+      setDelay(0);
+    } else {
+      setDelay(delay);
+    }
+  };
+
+  const goBack = (seconds: number) => {
+    audioRef.current!.currentTime = audioRef.current!.currentTime! - seconds;
+    handleAudioChange();
+  };
+
+  const goForward = (seconds: number) => {
+    audioRef.current!.currentTime = audioRef.current!.currentTime! + seconds;
+    handleAudioChange();
+  };
+
   return (
-    <video
-      ref={audioRef as unknown as React.RefObject<HTMLVideoElement>}
-      controls
-      autoPlay
-      
-    />
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      className="w-full max-w-xl p-6 bg-white rounded-lg shadow-md"
+    >
+      <audio ref={audioRef} />
+      <div className="flex items-center justify-center gap-4">
+        <Button onClick={() => goBack(1)} className="w-full mb-4 h-16 relative overflow-hidden">
+          <AnimatePresence initial={false} mode="wait">
+            <motion.div
+              key="back"
+              initial={{ y: 20, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              exit={{ y: -20, opacity: 0 }}
+              transition={{ duration: 0.2 }}
+              className="absolute inset-0 flex items-center justify-center"
+            >
+              <Rewind className="w-6 h-6" />
+            </motion.div>
+          </AnimatePresence>
+        </Button>
+        <Button onClick={togglePlay} className="w-full mb-4 h-16 relative overflow-hidden">
+          <AnimatePresence initial={false} mode="wait">
+            {!audioRef.current?.paused ? (
+              <motion.div
+                key="pause"
+                initial={{ y: -20, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                exit={{ y: 20, opacity: 0 }}
+                transition={{ duration: 0.2 }}
+                className="absolute inset-0 flex items-center justify-center"
+              >
+                <Pause className="w-6 h-6" />
+              </motion.div>
+            ) : (
+              <motion.div
+                key="play"
+                initial={{ y: 20, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                exit={{ y: -20, opacity: 0 }}
+                transition={{ duration: 0.2 }}
+                className="absolute inset-0 flex items-center justify-center"
+              >
+                <Play className="w-6 h-6" />
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </Button>
+        <Button onClick={() => goForward(1)} className="w-full mb-4 h-16 relative overflow-hidden">
+          <AnimatePresence initial={false} mode="wait">
+            <motion.div
+              key="forward"
+              initial={{ y: 20, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              exit={{ y: -20, opacity: 0 }}
+              transition={{ duration: 0.2 }}
+              className="absolute inset-0 flex items-center justify-center"
+            >
+              <FastForward className="w-6 h-6" />
+            </motion.div>
+          </AnimatePresence>
+        </Button>
+      </div>
+      <motion.div
+        className="mb-2 text-center"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.3, duration: 0.5 }}
+      >
+        <h1 className="text-2xl font-bold">Delay: {delay} segundos</h1>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, scale: 0.95 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ delay: 0.4, duration: 0.5 }}
+        className='h-20 flex flex-col items-center justify-center'
+      >
+
+        <audio
+          ref={audioRef}
+          onSeeked={handleAudioChange}
+          controls
+          autoPlay
+          className='w-full h-10'
+        />
+
+
+      </motion.div>
+    </motion.div>
+
   );
 }
 
